2e7c4dee995fd5970ee33ac61f9a983b9440616d
morojuindu4321 <morojuindu4321@gmail.com>
2026-06-23 07:23:22 +0000

    Add program 55 and program 56

---
+++ progarm55.py
@@ -0,0 +1,7 @@
+with open("/workspaces/ACE-BOOTCAMP-2026-06/OIP.jpg", "rb") as f:
+   content = f.read()
+with open("/workspaces/ACE-BOOTCAMP-2026-06/OIP1.jpg", "wb") as cf:
+   cf.write(content)
+         
+  
+  
---
+++ program56.py
@@ -0,0 +1,7 @@
+with open("/workspaces/ACE-BOOTCAMP-2026-06/Day5/program52.py", "r") as f:
+    content = f.readline()
+    print(content)
+    content = f.readline()
+    print(content)
+    content = f.readline()
+    print(content)
