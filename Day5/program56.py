aa9684207d2a4e3614522b64f4aa78b3fff5b4a2
morojuindu4321 <morojuindu4321@gmail.com>
2026-06-23 09:00:19 +0000

    Update Day5 program56

--- Day5/program56.py
+++ Day5/program56.py
@@ -1,7 +1,6 @@
-with open("/workspaces/ACE-BOOTCAMP-2026-06/Day5/program52.py", "r") as f:
-    content = f.readline()
-    print(content)
-    content = f.readline()
-    print(content)
-    content = f.readline()
-    print(content)
+with open("/workspaces/ACE-BOOTCAMP-2026-06/Day5/program52.py",'rb') as f:
+    content = f.readline(2000)
+    with open("/workspaces/ACE-BOOTCAMP-2026-06/Day5/program52.py","wb") as cf:
+        while  len(content) > 0:
+            cf.write(content)
+            content=f.read(2000)
